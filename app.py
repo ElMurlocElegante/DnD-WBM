88f70470119b0be90932f9d15c47a29ddd5e900d
martinamoio <mmolina@fi.uba.ar>
2024-06-08 18:43:07 -0300

    [feature][ID:2]login/register con engine
    creacion de la funcion delete_account(falta implementar logica) y limpieza de codigo con funciones que no se usan

--- app.py
+++ app.py
@@ -69,51 +69,18 @@ def home():
     if 'user_id' not in session:
         return redirect(url_for('login'))
     return render_template('home.html', username=session.get('username'))
-    
+
 @app.route('/logout')
 def logout():
-    logout_user()
+    session.clear()
+    flash('Sesión cerrada correctamente', 'success')
     return redirect(url_for('login'))
 
-@app.route('/users', methods=['GET'])
-def users():
-    users = User.query.all()
-    data = [{'id': u.id, 'username': u.username, 'email': u.email} for u in users]
-    return jsonify(data), 200
+@app.route('/delete_account', methods=['POST'])
+def delete_account():
+    """logica para eliminar un usuario"""
+    return redirect(url_for('login'))
 
-@app.route('/users/<id>', methods=['PATCH'])
-def update_user(id):
-    mod_user = request.get_json()
-    user = User.query.get(id)
-    if user:
-        user.username = mod_user.get('username', user.username)
-        user.email = mod_user.get('email', user.email)
-        try:
-            db.session.commit()
-            return jsonify({'message': 'Usuario actualizado correctamente'}), 200
-        except exc.SQLAlchemyError as err:
-            return jsonify({'message': 'Error al actualizar usuario: ' + str(err.__cause__)})
-    return jsonify({'message': 'Usuario no encontrado'}), 404
-
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-    if user:
-        data = {'id': user.id, 'username': user.username, 'email': user.email}
-        return jsonify(data), 200
-    return jsonify({'message': 'Usuario no encontrado'}), 404
-
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
-    if user:
-        try:
-            db.session.delete(user)
-            db.session.commit()
-            return jsonify({'message': 'Usuario eliminado correctamente'}), 202
-        except exc.SQLAlchemyError as err:
-            return jsonify({'message': 'Error al eliminar usuario: ' + str(err.__cause__)})
-    return jsonify({'message': 'Usuario no encontrado'}), 404
 
 if __name__ == '__main__':
     app.run("127.0.0.1", port=5000, debug=True)
