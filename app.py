f8d9e8cc6641c4f04325552ea682e07caddf2242
José Rafael Oberto Castellano Del Mar <125618730+JoSeokke@users.noreply.github.com>
2024-06-08 13:23:57 -0300

    [feature][ID9]
    Cambio logica

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request
+from random import randint
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        dados = []
-        for i in range(1, 5):
-            dado = request.form.get(f"dado{i}")
-            if dado and dado.startswith("d") and dado[1:].isdigit() and int(dado[1:]) != 0:
-                dados.append(int(dado[1:]))
-            else:
-                error = f"Dado incorrecto: {dado}. Debe tener el formato 'dn', donde 'n' es un número mayor que cero."
-                return render_template('index.html', error=error)
-        if len(dados) <= 4:
-            return render_template('resultado.html', dados=dados)
-        else:
-            error = "Solo se permiten hasta 4 dados."
-            return render_template('index.html', error=error)
-    return render_template('index.html')
+@app.route('/roll_dice', methods=['POST'])
+def roll_dice():
+    data = request.get_json()  # Obtener los datos JSON de la solicitud POST
+    dice = data.get('dice')  # Obtener el valor de 'dice' de los datos JSON
+    
+    def index(dice):
+        if dice.startswith("d"):
+            if dice[1:].isdigit():
+                num_caras = int(dice[1:])
+                resultado = randint(1, num_caras)
+                return jsonify({"resultado": resultado})
+        elif "d" in dice:
+            num_dados, num_caras = dice.split("d")
+            if num_dados.isdigit() and num_caras.isdigit():
+                num_dados = int(num_dados)
+                num_caras = int(num_caras)
+                if num_dados > 0 and num_caras > 0:
+                    resultados = [randint(1, num_caras) for _ in range(num_dados)]
+                    return jsonify({"resultados": resultados})
+        return jsonify({"error": "Formato de dice incorrecto. Debe ser 'dn' o 'ndn', donde 'n' es un número mayor que cero."}), 400
+    
+    return index(dice)
 
 if __name__ == '__main__':
     app.run(debug=True)
