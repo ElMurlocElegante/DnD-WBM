8f7391b4f0491ba4bdc5785b6e3e6f868b027f86
Luciano Celano <136203824+ElMurlocElegante@users.noreply.github.com>
2024-06-08 21:51:47 -0300

    Revert "ID2"

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for, flash, session
+from flask import Flask, jsonify, request, render_template, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
@@ -11,7 +11,7 @@ import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
-engine = create_engine("mysql+mysqlconnector://root@localhost:3307/DnD-WBM")
+engine = create_engine("mysql+mysqlconnector://root@localhost:3306/DnD-WBM")
 app.config['SECRET_KEY'] = 'SECRET'
 socketio = SocketIO(app)
 '''
@@ -44,92 +44,8 @@ def codeGenerator(lenght):
 
 @app.route("/")
 def home():
-    if session['room'] != None:
-        session.pop('room', None)
-    return render_template("home.html")
-
-# Account Things
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        query = "SELECT * FROM users WHERE username = :username AND password = :password;"
-        conn = engine.connect()
-        try:
-            result = conn.execute(text(query), {"username": username, "password": password}).fetchone()
-            conn.close()
-        except SQLAlchemyError as err:
-            flash(f"Error: {str(err.__cause__)}", 'danger')
-            return render_template('auth/login.html')
-        if result:
-            session['user_id'] = result.id  
-            session['username'] = result.username 
-            return redirect(url_for('home'))
-        else:
-            flash('Usuario o contraseña incorrecta', 'danger')
-            return render_template('auth/login.html')
-    else:
-        return render_template('auth/login.html')
-    
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        query_email = "SELECT * FROM users WHERE email = :email;"
-        query_username = "SELECT * FROM users WHERE username = :username;"
-        conn = engine.connect()
-        try:
-            result = conn.execute(text(query_email), {"email": email}).fetchone()
-            if result:
-                flash('El correo ya está en uso', 'danger')
-                conn.close()
-                return render_template('auth/register.html')
-            result_username = conn.execute(text(query_username), {"username": username}).fetchone()
-            if result_username:
-                flash('El nombre de usuario ya está en uso', 'danger')
-                conn.close()
-                return render_template('auth/register.html')
-            query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password);"
-            conn.execute(text(query), {"username": username, "email": email, "password": password})
-            conn.commit()
-            conn.close()
-            flash('Usuario registrado correctamente', 'success')
-            return redirect(url_for('login'))
-        except SQLAlchemyError as err:
-            flash(f"Error: {str(err.__cause__)}", 'danger')
-            return render_template('auth/register.html')
-    else:
-        return render_template('auth/register.html')
-
-
-@app.route('/logout')
-def logout():
     session.clear()
-    flash('Sesión cerrada correctamente', 'success')
-    return redirect(url_for('login'))
-
-@app.route('/delete_account', methods=['POST'])
-def delete_account():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-
-    user_id = session['user_id']
-    conn = engine.connect()
-    query = f"DELETE FROM users WHERE id = {user_id};"
-    try:
-        conn.execute(text(query))
-        conn.commit()
-        conn.close()
-        session.clear()
-        flash('Cuenta eliminada correctamente', 'success')
-    except SQLAlchemyError as err:
-        flash(f"Error: {str(err.__cause__)}", 'danger')
-    return redirect(url_for('login'))
+    return render_template("home.html")
 
 @app.route("/characters", methods=['GET'])
 def characters():
@@ -254,8 +170,7 @@ def roll_dice():
         return jsonify({"error": "Formato de dice incorrecto. Debe ser 'dn' o 'ndn', donde 'n' es un número mayor que cero."}), 400
     
     return index(dice)
-
-# Rooms
+  
 @app.route("/gameRooms")
 def gameRooms():
 
