731d5052a7c30d80d679800c37b887e867e333d5
Martina Molina Moio <mmolina@fi.uba.ar>
2024-06-11 18:52:34 -0300

    Revert "[refactor] login redirect"
    This reverts commit bb7d8018936802bd0d847811fb129f8ba920d289.

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def login():
         if result:
             session['user_id'] = result.id  
             session['username'] = result.username 
-            return redirect(url_for('characters'))
+            return redirect(url_for('home'))
         else:
             flash('Usuario o contraseña incorrecta', 'danger')
             return render_template('auth/login.html')
@@ -489,4 +489,4 @@ def message(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True)
