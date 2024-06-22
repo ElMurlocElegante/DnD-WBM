a386c3aa4dc20483dc32b30894e2e65ea1b620b5
Martina Molina Moio <mmolina@fi.uba.ar>
2024-06-22 16:05:31 -0300

    [style]
    Mensajes del login en ingles

--- frontend/app.py
+++ frontend/app.py
@@ -171,7 +171,7 @@ def login():
             session['username'] = username
             return redirect(url_for('characters'))
         else:
-            flash('Usuario o contraseña incorrecta', 'danger')
+            flash('Incorrect user or password', 'danger')
             return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
@@ -203,7 +203,7 @@ def register():
 @app.route('/logout')
 def logout():
     session.clear()
-    flash('Sesión cerrada correctamente', 'success')
+    flash('Session successufully closed', 'success')
     return render_template("home.html")
 
 #delete account
